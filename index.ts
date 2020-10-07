32dfce65495282e86db09655474a947c46b62d16
josemorista <josemorista@id.uff.br>
2020-10-06 22:16:00 -0300

    feat: random restart

--- index.ts
+++ index.ts
@@ -64,10 +64,10 @@ for (let i = 0; i < vertexCount; i++) {
 groundTruthArray = groundTruthArray.sort((a, b) => b.eval - a.eval);
 
 
-// Hill Climbing sideways to find our solutions
+// Hill Climbing sideways and with random restarts to find our solutions
 let solutionsArray: Array<{ solution: string, eval: number }> = [];
 for (let i = 0; i < 5; i++) {
-	solutionsArray.push(hillClimbing<string>({
+	const candidates = [...new Array(5)].map(() => hillClimbing<string>({
 		evalFunction,
 		initialState: {
 			solution: String(Math.round(Math.random() * vertexCount)),
@@ -77,8 +77,16 @@ for (let i = 0; i < 5; i++) {
 	}, {
 		performSideways: 100
 	}));
+	let bestCandidate = candidates[0];
+	candidates.forEach(candidate => {
+		if (candidate.eval > bestCandidate.eval) {
+			bestCandidate = candidate;
+		}
+	});
+	solutionsArray.push(bestCandidate);
 }
 solutionsArray = solutionsArray.sort((a, b) => b.eval - a.eval);
 
-// Logging our solutions
-console.log(groundTruthArray, solutionsArray);
+// Logging our solutions and groundTruth
+console.log('groundTruth:', groundTruthArray);
+console.log('solutions:', solutionsArray);
