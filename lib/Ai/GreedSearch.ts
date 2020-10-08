22ff5dc3125adce1cdb5ad59d846fe3a5f2051d8
josemorista <josemorista@id.uff.br>
2020-10-08 01:08:05 -0300

    feat: added otimizations to greedy search

--- lib/Ai/GreedSearch.ts
+++ lib/Ai/GreedSearch.ts
@@ -9,21 +9,52 @@ interface GreedSearchOptions {
 	maxIterations?: number;
 }
 
+type List = {
+	value: number;
+	state: any;
+	next: List;
+} | null;
+
+function addList(l: List, state: any, value: number): List {
+	if (!l) {
+		return {
+			value,
+			state,
+			next: null
+		};
+	}
+	if (value > l.value) {
+		return {
+			value,
+			state,
+			next: l
+		};
+	}
+	l.next = addList(l.next, state, value);
+	return l;
+}
+
 export const greedSearch = <S>({ heuristic, initialState: initialState, expandFunction, isSolution }: Problem<S>, { maxIterations }: GreedSearchOptions = {}): { solution: S, iterations: number } => {
 	const explored: Array<S> = [];
-	let frontier: Array<S> = [initialState];
+	let frontier: List | null = addList(null, initialState, heuristic(initialState));
 	let iterationsCount = 0;
 
 	while (true) {
 
-		const current = frontier.shift() || {} as S;
+		const current = frontier?.state;
+		console.log(frontier?.value);
+		frontier = frontier?.next || null;
+
 		// Optional premature end conditions
 		if ((isSolution && isSolution(current)) || (maxIterations && iterationsCount >= maxIterations)) return { solution: current, iterations: iterationsCount };
-		explored.push(current);
-		console.log(frontier.length);
-		frontier = [...frontier, ...expandFunction(current).filter(el => !explored.includes(el) && !frontier.includes(el))].sort((a, b) => heuristic(b) - heuristic(a));
 
-		if (frontier.length === 0) {
+		explored.push(current);
+
+		expandFunction(current).filter(el => !explored.includes(el)).forEach(candidate => {
+			frontier = addList(frontier, candidate, heuristic(candidate));
+		});
+
+		if (!frontier) {
 			return { solution: current, iterations: iterationsCount };
 		}
 
